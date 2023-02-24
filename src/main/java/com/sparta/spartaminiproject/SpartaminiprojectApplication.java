9aed21ff254da4d472b81319a7adc5805945345d
Wooram Hong <coadhs1@naver.com>
2023-02-24 18:39:48 +0900

    init

---
+++ src/main/java/com/sparta/spartaminiproject/SpartaminiprojectApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.spartaminiproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpartaminiprojectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpartaminiprojectApplication.class, args);
+    }
+
+}
